cd78764ee5d8bbb1f1fb7b3a091dd6f3c66418f9
s <s.looi2@outlook.com>
2024-09-17 02:42:12 +0900

    :sparkles: create ghpages build script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { createWebglRenderer } from './webgl'
 
 
