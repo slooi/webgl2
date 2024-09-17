ffdd781df23f9b15e9c7aa947507efb37b25d57a
s <s.looi2@outlook.com>
2024-09-18 01:21:20 +0900

    :sparkles: implement origin translation

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { createWebglRenderer } from './webgl'
 
 function App() {
   const [scale, setScale] = useState<number>(1)
+  const [originTranslation, setOriginTranslation] = useState<{ x: number, y: number, z: number }>({ x: 0, y: 0, z: 0 })
   const [translation, setTranslation] = useState<{ x: number, y: number, z: number }>({ x: 0, y: 0, z: 0 })
   const [rotation, setRotation] = useState<{ x: number, y: number, z: number }>({ x: 0, y: 0, z: 0 })
 
@@ -25,12 +26,13 @@ function App() {
 
     renderer.clear()
     renderer.scale(scale, scale)
+    renderer.translate(originTranslation.x, originTranslation.y, originTranslation.z)
     renderer.rotateX(rotation.x)
     renderer.rotateY(rotation.y)
     renderer.rotateZ(rotation.z)
     renderer.translate(translation.x, translation.y, translation.z)
     renderer.draw()
-  }, [scale, translation, rotation])
+  }, [scale, translation, rotation, originTranslation])
 
   return (
     <>
@@ -40,19 +42,27 @@ function App() {
           <p>scale : {scale}</p>
           <input value={scale} type="range" onChange={e => setScale(Number(e.target.value))} min="0" max="2" step=".1" />
           <p>rotate X: {rotation.x}</p>
-          <input value={rotation.x} type="range" onChange={e => setRotation(v => ({ ...v, x: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={rotation.x} type="range" onChange={e => setRotation(v => ({ ...v, x: Number(e.target.value) }))} min="0" max="360" step="1" />
           <p>rotate Y: {rotation.y}</p>
-          <input value={rotation.y} type="range" onChange={e => setRotation(v => ({ ...v, y: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={rotation.y} type="range" onChange={e => setRotation(v => ({ ...v, y: Number(e.target.value) }))} min="0" max="360" step="1" />
           <p>rotate Z: {rotation.z}</p>
-          <input value={rotation.z} type="range" onChange={e => setRotation(v => ({ ...v, z: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={rotation.z} type="range" onChange={e => setRotation(v => ({ ...v, z: Number(e.target.value) }))} min="0" max="360" step="1" />
           <p>translate x : {translation.x}</p>
-          <input value={translation.x} type="range" onChange={e => setTranslation(v => ({ ...v, x: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={translation.x} type="range" onChange={e => setTranslation(v => ({ ...v, x: Number(e.target.value) }))} min="-150" max="150" step="1" />
           <p>translate y : {translation.y}</p>
-          <input value={translation.y} type="range" onChange={e => setTranslation(v => ({ ...v, y: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={translation.y} type="range" onChange={e => setTranslation(v => ({ ...v, y: Number(e.target.value) }))} min="-150" max="150" step="1" />
           <p>translate z : {translation.z}</p>
-          <input value={translation.z} type="range" onChange={e => setTranslation(v => ({ ...v, z: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <input value={translation.z} type="range" onChange={e => setTranslation(v => ({ ...v, z: Number(e.target.value) }))} min="-150" max="150" step="1" />
+
+          <h3 style={{ marginTop: "1rem" }}>Origin Parameters</h3>
+          <p>origin translate x : {originTranslation.x}</p>
+          <input value={originTranslation.x} type="range" onChange={e => setOriginTranslation(v => ({ ...v, x: Number(e.target.value) }))} min="-150" max="150" step="1" />
+          <p>origin translate y : {originTranslation.y}</p>
+          <input value={originTranslation.y} type="range" onChange={e => setOriginTranslation(v => ({ ...v, y: Number(e.target.value) }))} min="-150" max="150" step="1" />
+          <p>origin translate z : {originTranslation.z}</p>
+          <input value={originTranslation.z} type="range" onChange={e => setOriginTranslation(v => ({ ...v, z: Number(e.target.value) }))} min="-150" max="150" step="1" />
         </div>
-      </div>
+      </div >
     </>
   )
 }
