4cbc7727d5f1a66581d3e29a12239550b81099a5
s <s.looi2@outlook.com>
2024-09-18 01:01:22 +0900

    :sparkles: add ui to rotate x,y,z

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { createWebglRenderer } from './webgl'
 function App() {
   const [scale, setScale] = useState<number>(1)
   const [translation, setTranslation] = useState<[number, number]>([0, 0])
-  const [rotation, setRotation] = useState<number>(0)
+  const [rotation, setRotation] = useState<{ x: number, y: number, z: number }>({ x: 0, y: 0, z: 0 })
 
   const [renderer, setRenderer] = useState<ReturnType<typeof createWebglRenderer> | null>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -25,7 +25,9 @@ function App() {
 
     renderer.clear()
     renderer.scale(scale, scale)
-    renderer.rotate(rotation)
+    renderer.rotateX(rotation.x)
+    renderer.rotateY(rotation.y)
+    renderer.rotateZ(rotation.z)
     renderer.translate(translation[0], translation[1])
     renderer.draw()
   }, [scale, translation, rotation])
@@ -37,8 +39,12 @@ function App() {
         <div style={{ margin: "1rem" }}>
           <p>scale : {scale}</p>
           <input value={scale} type="range" onChange={e => setScale(Number(e.target.value))} min="0" max="2" step=".1" />
-          <p>rotate : {rotation}</p>
-          <input value={rotation} type="range" onChange={e => setRotation(Number(e.target.value))} min="0" max="300" step="1" />
+          <p>rotate X: {rotation.x}</p>
+          <input value={rotation.x} type="range" onChange={e => setRotation(v => ({ ...v, x: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <p>rotate Y: {rotation.y}</p>
+          <input value={rotation.y} type="range" onChange={e => setRotation(v => ({ ...v, y: Number(e.target.value) }))} min="0" max="300" step="1" />
+          <p>rotate Z: {rotation.z}</p>
+          <input value={rotation.z} type="range" onChange={e => setRotation(v => ({ ...v, z: Number(e.target.value) }))} min="0" max="300" step="1" />
           <p>translate x : {translation[0]}</p>
           <input value={translation[0]} type="range" onChange={e => setTranslation(t => [Number(e.target.value), t[1]])} min="0" max="300" step="1" />
           <p>translate y : {translation[1]}</p>
